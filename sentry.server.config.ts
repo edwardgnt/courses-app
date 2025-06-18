198438500f433dfb0daa3c295faaf26ee00eaa07
Edward Güereque <edwardgnt@gmail.com>
2025-06-18 09:05:55 -0700

    Implement AI Integration

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://73efaa47ea5577506af92f05659ab355@o4509516232261632.ingest.us.sentry.io/4509516249497600",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
